9cecc599fb3009c79700f721745ad408f50eb1e8
Funncy <gywns41@naver.com>
2021-01-15 17:01:41 +0900

    First commit & login page

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao_clone/screen/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Kakao clone',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).accentColor,
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(width: 170, child: Image.asset('assets/logo.png')),
+              RaisedButton(
+                color: Colors.yellowAccent,
+                onPressed: () {},
+                child: Container(
+                  width: 300,
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Icon(Icons.ac_unit_rounded),
+                      ),
+                      Expanded(child: Center(child: Text("카카오톡 로그인")))
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
