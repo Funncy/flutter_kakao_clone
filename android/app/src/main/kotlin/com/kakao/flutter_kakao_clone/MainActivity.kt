b9e235742a3647e9459352779b85e4f01fd8b0e5
Funncy <gywns41@naver.com>
2021-01-15 19:01:40 +0900

    Added Firebase settings

--- android/app/src/main/kotlin/com/kakao/flutter_kakao_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/kakao/flutter_kakao_clone/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_kakao_clone
+package com.kakao.flutter_kakao_clone
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
